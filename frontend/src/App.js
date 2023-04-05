74ff5f5eee95c6f17968d50167d42124b1768ad2
Athiyen <dhineesh2802@gmail.com>
2023-04-05 13:53:19 +0800

    updated react-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -25,25 +25,25 @@ const App = () => {
           <p>Project page</p>
           <Router>
               <div>
-                  <Link style={padding} to="/project/spatial_layout">Spatial Layout</Link>
-                  <Link style={padding} to="/project/furnishing">Furnishing</Link>
-                  <Link style={padding} to="/project/appliances">Appliances</Link>
-                  <Link style={padding} to="/project/electrical">Electrical</Link>
-                  <Link style={padding} to="/project/concept_board">Concept Board</Link>
-                  <Link style={padding} to="/project/materials">Materials</Link>
-                  <Link style={padding} to="/project/2d_carpentry_detail_drawings">2D Carpentry Detail Drawings</Link>
-                  <Link style={padding} to="/project/3d_renderings">3D Renderings</Link>
+                  <Link style={padding} to="/project/{id}/spatial-layout">Spatial Layout</Link>
+                  <Link style={padding} to="/project/{id}/furnishing">Furnishing</Link>
+                  <Link style={padding} to="/project/{id}/appliances">Appliances</Link>
+                  <Link style={padding} to="/project/{id}/electrical">Electrical</Link>
+                  <Link style={padding} to="/project/{id}/concept-board">Concept Board</Link>
+                  <Link style={padding} to="/project/{id}/materials">Materials</Link>
+                  <Link style={padding} to="/project/{id}/2d-carpentry-detail-drawings">2D Carpentry Detail Drawings</Link>
+                  <Link style={padding} to="/project/{id}/3d-renderings">3D Renderings</Link>
               </div>
 
               <Routes>
-                  <Route path="/project/spatial_layout" element={<SpatialLayout />} />
-                  <Route path="/project/furnishing" element={<Furnishing />} />
-                  <Route path="/project/appliances" element={<Appliances />} />
-                  <Route path="/project/electrical" element={<Electrical />} />
-                  <Route path="/project/concept_board" element={<ConceptBoard />} />
-                  <Route path="/project/materials" element={<Materials />} />
-                  <Route path="/project/2d_carpentry_detail_drawings" element={<TwoDCarpentryDetailDrawings />} />
-                  <Route path="/project/3d_renderings" element={<ThreeDRenderings />} />
+                  <Route path="/project/:id/spatial-layout" element={<SpatialLayout />} />
+                  <Route path="/project/:id/furnishing" element={<Furnishing />} />
+                  <Route path="/project/:id/appliances" element={<Appliances />} />
+                  <Route path="/project/:id/electrical" element={<Electrical />} />
+                  <Route path="/project/:id/concept-board" element={<ConceptBoard />} />
+                  <Route path="/project/:id/materials" element={<Materials />} />
+                  <Route path="/project/:id/2d-carpentry-detail-drawings" element={<TwoDCarpentryDetailDrawings />} />
+                  <Route path="/project/:id/3d-renderings" element={<ThreeDRenderings />} />
               </Routes>
           </Router>
       </div>
