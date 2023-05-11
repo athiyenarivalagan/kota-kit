e224646c6449928fadf9bd0ae0e938e524f2e0a1
xiaogit00 <leibingguo@gmail.com>
2023-05-11 15:56:37 +0800

    Fixed page alignment issue

--- src/routes/Dashboard.js
+++ src/routes/Dashboard.js
@@ -1,9 +1,11 @@
+import React from 'react'
 import { Outlet, useLoaderData } from "react-router-dom"
 import { getProjects } from "../services/projects"
-
+import { Layout, theme } from 'antd'
 import AppHeader from "../components/common/AppHeader"
 import SideBar from "../components/common/SideBar"
-// import Footer from "../components/common/Footer"
+
+const { Header, Content, Sider } = Layout;
 
 export async function loader() {
     const projects = await getProjects()
@@ -12,15 +14,37 @@ export async function loader() {
 
 const Dashboard = () => {
     const { projects } = useLoaderData()
+    const {
+        token: { colorBgContainer },
+      } = theme.useToken();
 
     return (
-        <>
-            <AppHeader />
-            <SideBar projects={projects}/>
-            {/* <Footer /> */}
-            <Outlet />
-        </>
-    )
+        <Layout>
+          <AppHeader />
+          <Layout>
+            <Sider
+              width={300}
+              theme={'light'}
+              style={{
+                background: colorBgContainer,
+              }}
+            >
+             <SideBar projects={projects}/>
+            </Sider>
+            <Layout>
+              <Content
+                style={{
+                  margin: 0,
+                  minHeight: 280,
+                  background: colorBgContainer,
+                }}
+              >
+                <Outlet />
+              </Content>
+            </Layout>
+          </Layout>
+        </Layout>
+      )
 }
 
 export default Dashboard
